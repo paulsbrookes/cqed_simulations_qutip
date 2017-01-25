a8b90fc1921243374804f3cf81700fcbd05195c6
Paul Brookes <pbrookes@pang.phys.ucl.ac.uk>
2017-01-25 18:19:37 +0000

    Completed function for carrying out Monte Carlo sims.

--- monte_solve.py
+++ monte_solve.py
@@ -9,65 +9,51 @@ import matplotlib.pyplot as plt
 #                 args="5450 5500")
 
 class system_parameters:
-    def __init__(self, fc, fq, g, chi, kappa, gamma):
+    def __init__(self, fc, fq, g, chi, eps, fd, kappa, gamma):
         self.fc = fc
         self.fq = fq
         self.eps = eps
         self.g = g
         self.chi = chi
+        self.eps = eps
+        self.fd = fd
         self.gamma = gamma
         self.kappa = kappa
 
-class drive_parameters:
-    def __init__(self, eps, wd):
+class hilbert_dimensions:
+    def __init__(self, c_levels, t_levels):
+        self.c_levels = c_levels
+        self.t_levels = t_levels
 
-def solution(eps, wd, cavity_levels, transmon_levels):
+def hamiltonian(sys_params, hilbert_dims):
+    a = tensor(destroy(hilbert_dims.c_levels), qeye(hilbert_dims.t_levels))
+    sm = tensor(qeye(hilbert_dims.c_levels), destroy(hilbert_dims.t_levels))
+    H = -(sys_params.fc - sys_params.fd) * a.dag() * a - (sys_params.fq - sys_params.fd) * sm.dag() * sm \
+        + sys_params.chi * sm.dag() * sm * (sm.dag() * sm - 1) + sys_params.g * (a.dag() * sm + a * sm.dag()) \
+        + sys_params.eps * (a + a.dag())
+    return H
 
-    wc = 10.3641 # 10.5665 cavity frequency
-    wa = 9.49101  # atom frequency
-    g = 0.202  # coupling strength
-    kappa = 0.00146
-    gamma = 0.000833  # atom dissipation rate
-    pump = 0  # atom pump rate
-    time = 50000
-    chi = -0.097
-    snaps = 100
-    taus = linspace(0, time, snaps)
-    a = tensor(destroy(cavity_levels), qeye(transmon_levels))
-    sm = tensor(qeye(cavity_levels), destroy(transmon_levels))
-    H = -(wc - wd) * a.dag() * a - (wa - wd) * sm.dag() * sm + chi * sm.dag() * sm * (sm.dag() * sm - 1) + g * (a.dag() * sm + a * sm.dag()) + eps * (
-        a + a.dag())
+def collapse_operators(sys_params, hilbert_dims):
+    a = tensor(destroy(hilbert_dims.c_levels), qeye(hilbert_dims.t_levels))
+    sm = tensor(qeye(hilbert_dims.c_levels), destroy(hilbert_dims.t_levels))
+    c_ops = []
+    c_ops.append(np.sqrt(sys_params.kappa) * a)
+    c_ops.append(np.sqrt(sys_params.gamma) * sm)
+    return c_ops
 
-    # n_th = 0  # bath temperature in terms of excitation number
-    c_op_list = []
-
-    n_th_a = 0  # zero temperature
-    rate = kappa * (1 + n_th_a)
-    if rate > 0.0:
-        c_op_list.append(np.sqrt(rate) * a)
-
-    rate = kappa * n_th_a
-    if rate > 0.0:
-        c_op_list.append(np.sqrt(rate) * a.dag())
-
-    rate = gamma
-    if rate > 0.0:
-        c_op_list.append(np.sqrt(rate) * sm)
-
-    rate = pump
-    if rate > 0.0:
-        c_op_list.append(np.sqrt(rate) * sm.dag())
-
-    rho0 = tensor(basis(cavity_levels, 0), basis(transmon_levels, 0))
-
-    projections = [tensor(fock_dm(cavity_levels, x), qeye(transmon_levels)) for x in range(cavity_levels)]
-    output = mcsolve(H, rho0, taus, c_op_list, [], ntraj=100)
-
+def solution(sys_params, hilbert_dims, timings, initial_state, n_traj=50):
+    end_time = timings[0]
+    n_snaps = timings[1]
+    H = hamiltonian(sys_params, hilbert_dims)
+    c_ops = collapse_operators(sys_params, hilbert_dims)
+    snapshot_times = linspace(0, end_time, n_snaps)
+    output = mcsolve(H, initial_state, snapshot_times, c_ops, [], ntraj=n_traj)
     return output
 
-
 if __name__ == '__main__':
-    cavity_levels = 200;
-    transmon_levels = 4;
-    output = solution(0.03, 10.4, cavity_levels, transmon_levels)
-    qsave(output.states, 'test')
+    sys_params = system_parameters(10.3641, 9.49101, 0.202, -0.097, 0.03, 10.4, 0.00146, 0.000833)
+    hilbert_dims = hilbert_dimensions(200, 4)
+    timings = [500, 100]
+    initial_state = tensor(basis(hilbert_dims.c_levels, 0), basis(hilbert_dims.t_levels, 0))
+    output = solution(sys_params, hilbert_dims, timings, initial_state, 10)
+    qsave(output.states, 'test2')
